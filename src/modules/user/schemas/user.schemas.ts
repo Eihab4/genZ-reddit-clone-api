87513045d2bdb678eb2afa18eec8088e5b2af35b
Eihab Muhammed <eihabmuhammed56@gmail.com>
2025-04-19 20:26:31 +0200

    Added necessary user fields

---
+++ src/modules/user/schemas/user.schemas.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import  { Document } from 'mongoose';
+
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+}
+
+export type UserDocument = User & Document;
+export const UserSchema = SchemaFactory.createForClass(User);
