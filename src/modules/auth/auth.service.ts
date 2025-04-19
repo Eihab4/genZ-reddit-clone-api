90f7833ccae81b14ab0b01e91afbe42637a70a18
Eihab Muhammed <eihabmuhammed56@gmail.com>
2025-04-19 23:24:14 +0200

    Added bcrypt for hashing passwords

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../user/schemas/user.schemas';
 import { Model } from 'mongoose';
 import { RegisterRequestDto } from './dtos/requests/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,8 @@ export class AuthService {
      ) { }
     async register(registerPayload: RegisterRequestDto) {
         const { username, email, password } = registerPayload;
-        await this.userModel.create({ username, email, password });
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await this.userModel.create({ username, email, password: hashedPassword });
         return {message:"User Registered Successfully"};
     }
 }
