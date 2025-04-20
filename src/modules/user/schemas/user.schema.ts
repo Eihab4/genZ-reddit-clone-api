a8519fcf153984877680742dafe8c7dfda87a6b8
Eihab Muhammed <eihabmuhammed56@gmail.com>
2025-04-20 18:15:56 +0200

    Removed age field from user schema

--- src/modules/user/schemas/user.schema.ts
+++ src/modules/user/schemas/user.schema.ts
@@ -13,9 +13,6 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ required: true })
-  age: number;
-
   @Prop({ type: [String], default: [] })
   interests: string[];
 
