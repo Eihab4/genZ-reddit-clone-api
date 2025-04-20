2fe25a600e18a6bcb6069b26e5a683b721f5aa06
Eihab Muhammed <eihabmuhammed56@gmail.com>
2025-04-20 09:26:39 +0200

    removed unused function

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,8 +6,4 @@ import { User } from './schemas/user.schemas';
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-
-  async findByEmail(email: string): Promise<User | null> {
-    return this.userModel.findOne({ email }).exec();
-  }
 }
